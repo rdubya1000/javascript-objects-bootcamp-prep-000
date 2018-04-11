f36775895d99e780488a464e789f8ea56bf06fa2
rwofford0@gmail.com <rwofford0@gmail.com>
2018-04-11 17:10:27 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -2,7 +2,7 @@ var playlist = { 'Slowdive': 'Alison'}
 
 function updatePlaylist(playlist, artistsName, songTitle){
 
-  Object.assign({}, playlist, {'Phil Ochs': "Here's to the State of Mississippi"})
+  Object.assign({}, playlist, {'Phil Ochs': "Here's to the State of Mississippi", })
   
   return playlist
 }
