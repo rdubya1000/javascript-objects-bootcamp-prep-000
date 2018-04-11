cb0a1f6a03459bcc3b9e5d25dcee0ecb48bfc3d8
rwofford0@gmail.com <rwofford0@gmail.com>
2018-04-11 16:55:43 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,5 +1,5 @@
 var playlist = { 'Slowdive': 'Alison', 'My Bloody Valintine': 'Sometimes'}
 
-function updatePlaylist(playlist, 'Phil Ochs', "Here's to the State of Mississippi"){
+function updatePlaylist(playlist, artistsName, "Here's to the State of Mississippi"){
   return playlist
 }
