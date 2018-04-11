d19db8aff73ffc55bbf83419b87205f62dc119cf
rwofford0@gmail.com <rwofford0@gmail.com>
2018-04-11 16:13:05 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -3,4 +3,10 @@ var playlist = { Metallica: 'One'};
 playlist.Slowdive = 'Alison'
 playlist['My Bloody Valintine'] = 'Sometimes'
 
-playlist 
+function updatePlaylist (obj, key, value){
+  obj[key] = value
+  
+  return obj
+}
+
+updatePlaylistv(playlist, )
